6683da3fdc48d5edf81743336e65725804a4d7bf
wangsheng <sheng0866@126.com>
2018-12-14 23:54:46 +0800

    2018/12/14

---
+++ src/main/java/com/wsheng/suanfa/leecode/Solution507.java
@@ -0,0 +1,15 @@
+package com.wsheng.suanfa.leecode;
+
+/**
+ * @Auther: wsheng
+ * @Date: 2018/12/12 13:01
+ * @Description:
+ */
+public class Solution507 {
+
+
+    public boolean checkPerfectNumber(int num) {
+        if (num == 1 ) return false;
+
+    }
+}
