8401853615af5e2804d6b8e3a026fb360d8033af
wangsheng <sheng0866@126.com>
2019-01-28 22:42:59 +0800

    commit 693 int

---
+++ src/main/java/com/wsheng/suanfa/leecode/Int/Solution693.java
@@ -0,0 +1,19 @@
+package com.wsheng.suanfa.leecode.Int;
+
+/**
+ * @Auther: wsheng
+ * @Date: 2019/1/28 22:32
+ * @Description:
+ */
+public class Solution693 {
+
+
+    public boolean hasAlternatingBits(int n) {
+        String num = Integer.toBinaryString(n);
+        char[] chars = num.toCharArray();
+        for (int i=0;i<chars.length-1;i++){
+            if (chars[i] == chars[i+1]) return false;
+        }
+        return true;
+    }
+}
